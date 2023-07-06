3d75cce50e6d81b721cbbcc317dacbe118b641b6
eduvinagre <eduvinagre@gmail.com>
2023-07-06 18:02:41 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,3 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Social Media App",
-  description: "Made by @eduvinagre using Next.js, Typescript and Prisma",
-};
